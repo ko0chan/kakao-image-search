fb6867ad7a1466f9b6183dbb5f60c21ab9d30fa7
ko0chan <kychan1320@gmail.com>
2021-06-30 22:23:01 +0900

    feat(SearchImageItem.kt) :  Add SearchImageItem model

---
+++ app/src/main/java/com/kychan/kakaoimageapi/presentation/SearchImageItem.kt
@@ -0,0 +1,9 @@
+package com.kychan.kakaoimageapi.presentation
+
+import java.util.*
+
+data class SearchImageItem (
+    val imageUrl: String,
+    val displaySiteName: String,
+    val datetime: Date
+)
