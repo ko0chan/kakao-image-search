211b650f3f9125eab472c79728dcb9f182fc349f
ko0chan <kychan1320@gmail.com>
2021-06-30 20:39:00 +0900

    feat(...) : databinding 적용

--- app/src/main/java/com/kychan/kakaoimageapi/MainActivity.kt
+++ app/src/main/java/com/kychan/kakaoimageapi/MainActivity.kt
@@ -2,10 +2,17 @@ package com.kychan.kakaoimageapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.kychan.kakaoimageapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
     }
 }
