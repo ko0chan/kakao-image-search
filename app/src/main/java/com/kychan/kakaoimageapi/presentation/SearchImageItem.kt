d7bc9f59666b0adf02c174da98fbf12e0e5749d6
ko0chan <kychan1320@gmail.com>
2021-07-01 21:54:20 +0900

    feat(SearchImageItem.kt) : Serializable 직렬화

--- app/src/main/java/com/kychan/kakaoimageapi/presentation/SearchImageItem.kt
+++ app/src/main/java/com/kychan/kakaoimageapi/presentation/SearchImageItem.kt
@@ -1,9 +1,10 @@
 package com.kychan.kakaoimageapi.presentation
 
+import java.io.Serializable
 import java.util.*
 
-data class SearchImageItem (
+data class SearchImageItem(
     val imageUrl: String,
     val displaySiteName: String,
     val datetime: Date
-)
+) : Serializable
